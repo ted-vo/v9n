b1f5ccecfe5b450a136922a164c7dacacbc30ce4
Sylire <V9N@MB14-ILT.local>
2022-04-01 14:27:26 +0700

    feat: init project

---
+++ src/components/ComingSoon.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+defineProps<{
+  author: string;
+  year: string;
+}>();
+</script>
+
+<style>
+@import "../assets/CommingSoon.css";
+</style>
+
+<template>
+  <div>
+    <div class="header">
+      <h1 class="author">{{ author }} - {{ year }}</h1>
+    </div>
+    <div class="center">
+      <span class="title">This site is</span>
+      <span class="content">Coming Soon</span>
+      <div class="social">
+        <ul>
+          <li>
+            <a href="https://github.com/ted-v9n" target="blank">
+              <font-awesome-icon :icon="['fab', 'github']" />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+
+import App from "./App.vue";
+import router from "./router";
+
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
+library.add(faGithub);
+
+const app = createApp(App)
+  .use(createPinia())
+  .use(router)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
+
